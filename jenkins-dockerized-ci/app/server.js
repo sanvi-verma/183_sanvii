b97da4536c2f18b177d9271008348eb9c5c3c125
Sanvi Verma <137278991+sanvi-verma@users.noreply.github.com>
2025-08-29 13:55:28 +0530

    Committed Changes

--- jenkins-dockerized-ci/app/server.js
+++ jenkins-dockerized-ci/app/server.js
@@ -1,6 +1,10 @@
 const http = require('http');
+
 const port = process.env.PORT || 5000;
+
 const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'content-type': 'text/plain' });
   res.end('Hello from Dockerized CI/CD via Jenkins!');
 });
+
 server.listen(port, () => console.log(`Server running on ${port}`));
